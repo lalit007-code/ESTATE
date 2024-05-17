d194d7b736c2e8565ec8f63f8752fbf4e445279f
Lalit Singh <lalit284546@gmail.com>
2024-05-17 11:40:30 +0530

    connnect to db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("db connected successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("server is runnin in 3000");
-  
 });
