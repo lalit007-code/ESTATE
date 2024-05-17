a1b780c9f08513e852d2655199a3af0e4930f7e6
Lalit Singh <lalit284546@gmail.com>
2024-05-17 10:33:44 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Profile from "./pages/Profile.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import About from "./pages/About.jsx";
 import SignIn from "./pages/SignIn.jsx";
+import Header from "./components/Header.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom"; //without refreshing the page
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Lalit</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search... "
+            className="bg-transparent focus:outline-none w-24 sm:w-64 "
+          />
+          <FaSearch className="text-slate=500" />
+        </form>
+        <ul className="flex gap-4 ">
+          <Link to="/">
+            <li className="hidden sm:inline text-color-700 hover:underline cursor-pointer ">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-color-700 hover:underline cursor-pointer ">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="hidden  text-color-700 hover:underline cursor-pointer ">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
