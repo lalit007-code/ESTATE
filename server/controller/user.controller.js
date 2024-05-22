b97496c95d99858b2330e6df12d69431f7a29ea2
Lalit Singh <lalit284546@gmail.com>
2024-05-22 13:10:18 +0530

    create get user listing route

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../modals/listing.modal.js";
 import User from "../modals/user.modal.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcrypt";
@@ -57,5 +58,18 @@ export const deleteUser = async (req, res, next) => {
     });
   } catch (error) {
     new error();
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listing = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listing);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listing"));
   }
 };
