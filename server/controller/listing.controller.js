fbbc20b3f38b1c5ff0aae1952d3cd8229a2116f4
Lalit Singh <lalit284546@gmail.com>
2024-05-24 17:15:02 +0530

    created serach api route

--- server/controller/listing.controller.js
+++ server/controller/listing.controller.js
@@ -66,3 +66,54 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startingIndex = parseInt(req.query.startingIndex) || 0;
+    let offer = req.query.offer;
+    //if my offer is not given in req query so  have to serach for it
+    //in db for all results including or excluding this
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const serachTerm = req.query.serachTerm || " ";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: serachTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({
+        [sort]: order,
+      })
+      .limit(limit)
+      .skip(startingIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
