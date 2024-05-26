cacb3d4819949f4e7c1702c734ca7dc23df61704
Lalit Singh <lalit284546@gmail.com>
2024-05-26 21:15:13 +0530

    typo fixed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -82,7 +82,7 @@ const SignIn = () => {
       <div className="flex gap-2 mt-5 ">
         <p>Dont Have an account?</p>
         <Link to={"/sign-up"}>
-          <span className="text-blue-700 ">Sign in</span>
+          <span className="text-blue-700 ">Sign Up</span>
         </Link>
       </div>
       {/* {error && <p className="text-red-500 mt-5">{error}</p>} */}
